df48b9c4bdf8cb498557942eb25e3bde52a3db34
Rogerio Alves <sigmamix10@gmail.com>
2023-02-25 14:50:00 -0300

    Reiniciando projeto

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,73 +19,76 @@
     <title>Netflix - Projeto Cópia</title>
 </head>
 <body class="bg-black">
-<img src="{{asset('images/fundo-netflix.jpg')}}" class="bg-image">
-<section>
-    <header class="header-principal">
-        <div class="logo">
-            <a href="#">
-                <svg viewBox="0 0 111 30" data-uia="netflix-logo" class="svg-icon svg-icon-netflix-logo" aria-hidden="true" focusable="false">
-                    <g id="netflix-logo">
-                        <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z" id="Fill-14">
-                        </path>
-                    </g>
-                </svg>
-            </a>
-        </div>
-        <div class="nav-menu">
-        </div>
-    </header>
-    <section class="box-login">
-        <h1 class="login-title">Entrar</h1>
-        <form action="#" class="form-login">
-            @csrf
-            <label for="user" class="form-login-label">
-                <input type="text" name="user" class="form-login-input">
-            </label>
-            <label for="password" class="form-login-label">
-                <input type="password" name="password" class="form-login-input">
-            </label>
-            <button type="submit" class="form-login-btn">Entrar</button>
-        </form>
+    <img src="{{asset('images/fundo-netflix.jpg')}}" class="bg-image">
+    <section>
+        <header class="header-principal">
+            <div class="logo">
+                <a href="#">
+                    <svg viewBox="0 0 111 30" data-uia="netflix-logo" class="svg-icon svg-icon-netflix-logo" aria-hidden="true" focusable="false">
+                        <g id="netflix-logo">
+                            <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z" id="Fill-14">
+                            </path>
+                        </g>
+                    </svg>
+                </a>
+            </div>
+            <div class="nav-menu">
+            </div>
+        </header>
+        <section class="box-login">
+            <h1 class="login-title">Entrar</h1>
+            <form action="#" class="form-login">
+                @csrf
+                <label for="user" class="form-login-label">
+                    <span>Email ou número de telefone</span>
+                    <input type="text" name="user" class="form-login-input">
+                </label>
+                <label for="password" class="form-login-label">
+                    <span>Senha</span>
+                    <input type="password" name="password" class="form-login-input">
+                    <button class="btn">Mostrar</button>
+                </label>
+                <button type="submit" class="form-login-btn">Entrar</button>
+            </form>
+        </section>
     </section>
-</section>
-<footer class="opacidade-40">
-    <div class="footer-duvidas">Dúvidas? Ligue <span class="footer-tel">0800 591 8942</span></div>
-    <div class="footer-links">
-        <div class="footer-links-col">
-            <a class="footer-links-col-item">Perguntas frequentes</a>
-            <a class="footer-links-col-item">Preferências de cookies</a>
+    <footer class="opacidade-40">
+        <div class="footer-duvidas">Dúvidas? Ligue <span class="footer-tel">0800 591 8942</span></div>
+        <div class="footer-links">
+            <div class="footer-links-col">
+                <a class="footer-links-col-item">Perguntas frequentes</a>
+                <a class="footer-links-col-item">Preferências de cookies</a>
+            </div>
+            <div class="footer-links-col">
+                <a class="footer-links-col-item">Central de Ajuda</a>
+                <a class="footer-links-col-item">Informações corporativas</a>
+            </div>
+            <div class="footer-links-col">
+                <a class="footer-links-col-item">Termo de uso</a>
+            </div>
+            <div class="footer-links-col">
+                <a class="footer-links-col-item">Privacidade</a>
+            </div>
         </div>
-        <div class="footer-links-col">
-            <a class="footer-links-col-item">Central de Ajuda</a>
-            <a class="footer-links-col-item">Informações corporativas</a>
+        <div class="footer-lang">
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-world" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <circle cx="12" cy="12" r="9"></circle>
+                    <line x1="3.6" y1="9" x2="20.4" y2="9"></line>
+                    <line x1="3.6" y1="15" x2="20.4" y2="15"></line>
+                    <path d="M11.5 3a17 17 0 0 0 0 18"></path>
+                    <path d="M12.5 3a17 17 0 0 1 0 18"></path>
+                </svg>
+                <span>Português</span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-caret-down" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <circle cx="12" cy="12" r="9"></circle>
+                    <path d="M12 15l-4 -4h8z"></path>
+                </svg>
+            </button>
         </div>
-        <div class="footer-links-col">
-            <a class="footer-links-col-item">Termo de uso</a>
-        </div>
-        <div class="footer-links-col">
-            <a class="footer-links-col-item">Privacidade</a>
-        </div>
-    </div>
-    <div class="footer-lang">
-        <button>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-world" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <circle cx="12" cy="12" r="9"></circle>
-                <line x1="3.6" y1="9" x2="20.4" y2="9"></line>
-                <line x1="3.6" y1="15" x2="20.4" y2="15"></line>
-                <path d="M11.5 3a17 17 0 0 0 0 18"></path>
-                <path d="M12.5 3a17 17 0 0 1 0 18"></path>
-            </svg>
-            <span>Português</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-caret-down" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <circle cx="12" cy="12" r="9"></circle>
-                <path d="M12 15l-4 -4h8z"></path>
-            </svg>
-        </button>
-    </div>
-</footer>
+    </footer>
 
 <script src="{{asset('js/button-faq-action.js')}}"></script>
 </body>
